30c0c7c83d0d463ec86368c6892d95dfc0696e21
vmusulainen <vmusulainen@gmail.com>
2017-01-19 21:15:46 +0300

    renasming report to inventoryItem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20170116144943) do
+ActiveRecord::Schema.define(:version => 20170119144943) do
 
   create_table "customers", :force => true do |t|
     t.string   "first"
