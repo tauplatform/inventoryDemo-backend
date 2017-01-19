114d21eb395bd87af85e9418dd5d1e6d74eabb6c
vmusulainen <vmusulainen@gmail.com>
2017-01-19 03:01:50 +0300

    update uploading files

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -9,20 +9,22 @@ class UploadController < ApplicationController
   # end
 
   def create
-    extension = File.extname(params[:file].original_filename)
+    puts "params #{params}"
+    extension = File.extname(params[:filename])
     filename = "#{SecureRandom.uuid}#{extension}"
     dirname = File::join(Rails.public_path, 'images', 'upload')
+    path = File::join(Rails.public_path, 'images', 'upload', filename)
+
+    puts "filename: #{path}"
+    puts "filename: https://taustore.herokuapp.com/images/upload/#{filename}"
 
     unless File.directory?(dirname)
       puts "uploading: create uploading directory #{dirname}"
       FileUtils.mkdir_p(dirname)
     end
 
-    path = File::join(Rails.public_path, 'images', 'upload', filename)
     File.open(path, 'wb') { |f| f.write(params[:file].read) }
-    #redirect_to :action => :new, :filename => "https://taustore.herokuapp.com/images/upload/#{filename}"
-    puts "filename: #{path}"
-    puts "filename: https://taustore.herokuapp.com/images/upload/#{filename}"
+
     respond_to do |format|
       msg = {:status => 200, :filename => "https://taustore.herokuapp.com/images/upload/#{filename}"}
       format.json { render :json => msg } # don't do msg.to_json
