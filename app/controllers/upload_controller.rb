196ff03aaae08c80f7f71f99feba478f80da07c3
vmusulainen <vmusulainen@gmail.com>
2017-01-20 12:09:59 +0300

    Public server URL is changed from https to http

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -20,7 +20,7 @@ class UploadController < ApplicationController
     path = File::join(Rails.public_path, 'images', 'upload', filename)
 
     puts "filename: #{path}"
-    puts "filename: https://taustore.herokuapp.com/images/upload/#{filename}"
+    puts "filename: http://taustore.herokuapp.com/images/upload/#{filename}"
 
     unless File.directory?(dirname)
       puts "uploading: create uploading directory #{dirname}"
@@ -30,7 +30,7 @@ class UploadController < ApplicationController
     File.open(path, 'wb') { |f| f.write(params[:file].read) }
 
     respond_to do |format|
-      msg = {:status => 200, :filename => "https://taustore.herokuapp.com/images/upload/#{filename}"}
+      msg = {:status => 200, :filename => "http://taustore.herokuapp.com/images/upload/#{filename}"}
       format.json { render :json => msg } # don't do msg.to_json
     end
   end
