8edf8e668b4ba720b7e68f8cf117ce0af2dfbf2f
vmusulainen <vmusulainen@gmail.com>
2017-03-16 00:17:57 +0300

    Never override the initialize method in ActiveRecord

--- app/controllers/inventory_items_controller.rb
+++ app/controllers/inventory_items_controller.rb
@@ -2,7 +2,7 @@ require 'aws-sdk'
 
 class InventoryItemsController < ApplicationController
 
-  def initialize
+  def after_initialize
     s3 = Aws::S3::Resource.new(
         credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
         region: ENV['AWS_REGION']
